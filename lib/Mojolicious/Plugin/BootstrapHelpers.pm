fa29f660cbfd5e87f86730371e43573850c2c309
Quim Rovira <quim@rovira.cat>
2014-04-19 21:02:54 +0200

    Prefix all helpers with bs_ (for bullshit) Add the bs_alert helper to quickly generate divs with the alert classes Add flash and notification helpers, to shorcut some common uses

--- lib/Mojolicious/Plugin/BootstrapHelpers.pm
+++ lib/Mojolicious/Plugin/BootstrapHelpers.pm
@@ -2,13 +2,22 @@ package Mojolicious::Plugin::BootstrapHelpers;
 use Mojo::Base 'Mojolicious::Plugin';
 
 use Mojo::ByteStream;
+use Mojo::Util 'xml_escape';
 
 our $VERSION = '0.01';
 
 sub register {
   my ($self, $app) = @_;
 
-  $app->helper( form_group => \&_form_group );
+  # Tag generators
+  $app->helper( bs_form_group => \&_form_group );
+  $app->helper( bs_alert => \&_alert );
+
+  # Flash message helpers
+  $app->helper( bs_flash => \&_bs_flash );
+  $app->helper( bs_notify => \&_bs_notify );
+  $app->helper( bs_flash_to => \&_bs_flash_to );
+  $app->helper( bs_all_flashes => \&_bs_all_flashes );
 }
 
 
@@ -38,6 +47,72 @@ sub _form_group {
     return $self->tag( "div", %attrs, defined($ct) ? sub { $ct } : () );
 }
 
+sub _alert {
+    my ($self, $class) = (shift, shift);
+
+    my $cb = ref $_[-1] eq 'CODE' ? pop : undef;
+
+    my $content = @_ % 2 ? pop : undef;
+
+    my $ct = $cb ? $cb->() : $content ? xml_escape($content) : undef;
+
+    my %attrs = @_;
+
+    if ($attrs{dismissable}) {
+        $ct //= '';
+        $ct = $self->button( class => "close", "data-dismiss" => "alert", "aria-hidden" => "true", '&times;' );
+        $ct .= $ct;
+        delete $attrs{dismissable};
+    }
+
+    $attrs{class} .= $attrs{class} ? " alert alert-$class" : "alert alert-$class";
+
+    return $self->tag( "div", %attrs, defined($ct) ? sub { $ct } : () );
+}
+
+
+
+sub _bs_flash {
+    my ($self, $class, $message) = @_;
+    # Ugly hack accessing new_flash directly, to avoid a fetch/reset cycle
+    my $current = $self->session->{new_flash}{bs_flashes} //= [];
+
+    push @$current, [ $class => $message ];
+
+    $self->flash( bs_flashes => $current );
+}
+
+sub _bs_notify {
+    my ($self, $class, $message) = @_;
+    my $current = $self->stash('bs_notifications') // [];
+
+    push @$current, [ $class => $message ];
+
+    $self->stash( bs_notifications => $current );
+}
+
+sub _bs_flash_to {
+    my ($self, $class, $message, @redirect) = @_;
+
+    $self->bs_flash( $class, $message );
+
+    $self->redirect_to( @redirect );
+}
+
+sub _bs_all_flashes {
+    my ($self, $class, $message, @redirect) = @_;
+    my $flashes = $self->flash('bs_flashes') // [];
+    my $notifications = $self->stash('bs_notifications') // [];
+    my $content = '';
+
+    foreach my $f (@$flashes, @$notifications) {
+        $content .= $self->bs_alert( @$f );
+    }
+
+    return Mojo::ByteStream->new($content);
+}
+
+
 1;
 __END__
 
@@ -63,13 +138,13 @@ helpers for L<Mojolicious>.
 
 =head1 HELPERS
 
-=head2 form_group
+=head2 bs_form_group
 
-  %= form_group 'email' => begin
+  %= bs_form_group 'email' => begin
   %= email_field 'email'
   % end
 
-  %= form_group 'email' => ( label => "e-Mail address" ) => begin
+  %= bs_form_group 'email' => ( label => "e-Mail address" ) => begin
   %= text_field 'email'
   % end
 
@@ -87,6 +162,54 @@ You can also provide a "label" attribute to autogenerate it.
     <label for="email" class="control-label">e-Mail address</label>
     <input name="email"type="email" />
   </div>
+
+=head2 bs_alert
+
+  %= bs_alert success => "Operation completed"
+  %= bs_alert danger => ( dismissable => 1 ) => begin
+    <span>Something went terribly wrong</span>
+  % end
+
+Generates a div wrapper with the alert and alert-$class classes.
+
+  <div class="alert alert-success">Operation completed</siv>
+  <div class="alert alert-danger">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <span>Something went terribly wrong</span>
+  </div>
+
+=head1 FLASH AND NOTIFICATION HELPERS
+
+=head2 bs_flash
+
+  $self->bs_flash( warning => "Beware of the dog" );
+  $self->bs_flash( danger => "The dog is attqacking you!" );
+  $self->bs_flash( success => "The dog killed you!" );
+
+A stacking flash notification helper, similar to the regular flash. It can be
+called multiple times, to stack different messages.
+
+=head2 bs_notify
+
+  $self->bs_notify( warning => "Beware of the dog" );
+  $self->bs_notify( danger => "The dog is attqacking you!" );
+  $self->bs_notify( success => "The dog killed you!" );
+
+Queues notifications for the user using the stash instead of the session.
+
+=head2 bs_flash_to
+
+  $self->bs_flash( warning => "Beware of the dog", '/to/some/action' );
+
+Stacks a flash notification and calls redirect_to(...) with any additional
+arguments passed to this helper.
+
+=head2 bs_all_flashes
+
+  <%= b_all_flashes %>
+
+This method will call the bs_alert helper with each notification found,
+both on the flashed notifications or the stash ones.
 
 =head1 METHODS
 
